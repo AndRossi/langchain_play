086a613c2156e48a1153c77dabb6c693bd142019
androssi <and.rossi.516@gmail.com>
2024-06-15 21:59:40 +0200

    add a populate_pinecone.py script to create and populate a vertex store index in Pinecone

--- populate_pinecone.py
+++ populate_pinecone.py
@@ -3,6 +3,7 @@ import os
 
 from pinecone import Pinecone, ServerlessSpec
 from keys import PINECONE_KEY, OPENAI_KEY
+from constants import PINECONE_INDEX_NAME
 import time
 from langchain_pinecone import PineconeVectorStore
 
@@ -11,14 +12,12 @@ from langchain_openai import OpenAIEmbeddings
 from langchain_text_splitters import CharacterTextSplitter
 
 DATA_FOLDER = os.path.abspath("data")
-INDEX_NAME = "playground-index-test"  # change if desired
 
 # Endpoint to Pinecone API
 pc = Pinecone(api_key=PINECONE_KEY)
 
 # Set the PINECONE_KEY in the environment.
-# This is not needed for index creation/management APIs (we use the pc endpoint, that we initialized with the api key)
-# but it is needed for PineconeVectorStore operations that upsert chunked docs into the vector store.
+# This is needed for PineconeVectorStore operations that upsert chunked docs into the vector store.
 os.environ["PINECONE_API_KEY"] = PINECONE_KEY
 
 # Load all documents and split them into 1000-sized chunks.
@@ -40,21 +39,21 @@ embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_KEY)
 # Create a new
 print(f"Connecting to Pinecone...")
 existing_indexes = [index_info["name"] for index_info in pc.list_indexes()]
-if INDEX_NAME not in existing_indexes:
-    print(f"\tNo index {INDEX_NAME} found. Creating it...")
+if PINECONE_INDEX_NAME not in existing_indexes:
+    print(f"\tNo index {PINECONE_INDEX_NAME} found. Creating it...")
     pc.create_index(
-        name=INDEX_NAME,
+        name=PINECONE_INDEX_NAME,
         dimension=1536,
         metric="cosine",
         spec=ServerlessSpec(cloud="aws", region="us-east-1"),
     )
-    while not pc.describe_index(INDEX_NAME).status["ready"]:
+    while not pc.describe_index(PINECONE_INDEX_NAME).status["ready"]:
         time.sleep(1)
     print(f"\tDone.\n")
 
-    index = pc.Index(INDEX_NAME)
+    index = pc.Index(PINECONE_INDEX_NAME)
 
     # Upload into the pinecone vector store
     print(f"\tUploading docs into Pinecone...")
-    docsearch = PineconeVectorStore.from_documents(chunked_documents, embeddings, index_name=INDEX_NAME)
+    docsearch = PineconeVectorStore.from_documents(chunked_documents, embeddings, index_name=PINECONE_INDEX_NAME)
     print(f"\tDone.\n")
